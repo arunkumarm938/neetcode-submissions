2a704f5684968442b492ddca7949f0fca4c139f2
arunkumarm938 <vijiaakaash@gmail.com>
2026-09-18 21:08:06 +0530

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> sol = new ArrayList<>();
+        Map<Character, int[]> map= new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), new int[2]);
+                map.get(s.charAt(i))[0] = i;
+            }
+            map.get(s.charAt(i))[1] = i;
+        }
+        List<int[]> list = new ArrayList<>(map.values());
+        Set<Character> set = new HashSet<>();
+        for (int i=0;i<list.size();i++) {
+            int start = list.get(i)[0];
+            if(set.contains(s.charAt(start))){
+                continue;
+            }
+            set.add(s.charAt(start));
+            int end = list.get(i)[1];
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(j)[0] > start && list.get(j)[0] < end){
+                    set.add(s.charAt(list.get(j)[0]));
+                    if(list.get(j)[1] > end){
+                        end = list.get(j)[1];
+                    }
+                }
+            }
+            sol.add(end-start + 1);
+        }
+        return sol;
+    }
+}
